7e90fe6480c14b43e7c4b3fde413f78ee888f0f1
ilkem.erol <ilkem.erol@hotmail.com>
2018-07-08 00:09:06 +0300

    Top 10 visual improvement

--- src/Apis/top10.js
+++ src/Apis/top10.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import '../css/loader.css';
-import firstPlace from '../images/leaderboard_1.png';
+import firstPlaceImg from '../images/leaderboard_1.png';
 
 const API = 'http://localhost:8080/topTen';
 const DEFAULT_QUERY = '';
@@ -38,36 +38,132 @@ const withFetching = (url) => (Comp) =>
     }
   }
 
-const BrDailyStore = ({ data, isLoading, error }) => {
+const top10 = ({ data, isLoading, error }) => {
   const hits = data.entries || [];
 
-  function isTopThree(elem){
-      if(elem == "1"){
-          return "first";
-      } else if(elem == "2"){
-          return "second";
-      } else if(elem == "3"){
-          return "third";
-      } else {
-          return "defaultLeaderboard";
-      }
+  function firstPlace(elem) {
+    let board = []
+
+    elem = elem.slice(0, 1);
+
+    {elem.map(e=>
+      board.push(<div className="col-lg-4">
+      <div className="margin-left-right-5 top10Border first">
+        <img className="padding-20" src={firstPlaceImg} style={{width: '100%'}}/>
+        <h1 className="font-style text-center text-white">{e.username}</h1>
+        <hr className="hr margin-bottom-30" />
+        <h4 className="font-style text-center text-white">Kills: {e.kills}</h4>
+        <h4 className="font-style text-center text-white">Wins: {e.wins}</h4>
+        <h4 className="font-style text-center text-white">K/D: {e.kd}</h4>
+        <h4 className="font-style text-center text-white">Matches: {e.matches}</h4>
+        <h4 className="font-style text-center text-white">TTP: {e.minutes/60} H</h4>
+      </div>
+      </div>
+      )
+    )}
+
+    return board
   }
 
-  function allowDisplayImg(elem){
-    if(elem == "1"){
-        return "allowDisplay";
-    } else {
-        return "notAllowDisplay";
-    }
-   }
+  function secondPlace(elem) {
+    let board = []
 
-   function allowDisplayTxt(elem){
+    elem = elem.slice(1, 2);
+
+    {elem.map(e=>
+      board.push(<div className="col-lg-4">
+        <div className="margin-left-right-5 top10Border second">
+        <img className="padding-20" src={firstPlaceImg} style={{width: '100%'}}/>
+        <h1 className="font-style text-center text-white">{e.username}</h1>
+        <hr className="hr margin-bottom-30" />
+        <h4 className="font-style text-center text-white">Kills: {e.kills}</h4>
+        <h4 className="font-style text-center text-white">Wins: {e.wins}</h4>
+        <h4 className="font-style text-center text-white">K/D: {e.kd}</h4>
+        <h4 className="font-style text-center text-white">Matches: {e.matches}</h4>
+        <h4 className="font-style text-center text-white">TTP: {e.minutes/60} H</h4>
+      </div>
+      </div>
+      )
+    )}
+
+    return board
+
+  }
+
+  function thirdPlace(elem) {
+    let board = []
+
+    elem = elem.slice(2, 3);
+
+    {elem.map(e=>
+      board.push(<div className="col-lg-4">
+        <div className="margin-left-right-5 top10Border third">
+        <img className="padding-20" src={firstPlaceImg} style={{width: '100%'}}/>
+        <h1 className="font-style text-center text-white">{e.username}</h1>
+        <hr className="hr margin-bottom-30" />
+        <h4 className="font-style text-center text-white">Kills: {e.kills}</h4>
+        <h4 className="font-style text-center text-white">Wins: {e.wins}</h4>
+        <h4 className="font-style text-center text-white">K/D: {e.kd}</h4>
+        <h4 className="font-style text-center text-white">Matches: {e.matches}</h4>
+        <h4 className="font-style text-center text-white">TTP: {e.minutes/60} H</h4>
+      </div>
+      </div>
+      )
+    )}
+
+    return board
+
+  }
+
+  function otherPlayer(elem) {
+    let table = []
+    let innerTable = []
+
+    elem = elem.slice(3, elem.length);
+
+    {elem.map(e =>
+      innerTable.push(<tbody>
+        <tr>
+            <th className="font-style letter-spacing-2 text-white text-center" scope="row"><p>{e.rank}</p></th>
+            <td className="font-style letter-spacing-2 text-white text-center">{e.username}</td>
+            <td className="font-style letter-spacing-2 text-white text-center">{e.kills}</td>
+            <td className="font-style letter-spacing-2 text-white text-center">{e.wins}</td>
+            <td className="font-style letter-spacing-2 text-white text-center">{e.kd}</td>
+            <td className="font-style letter-spacing-2 text-white text-center">{e.platform}</td>
+        </tr>
+    </tbody>)
+    )}
+
+    table.push(<div className="row margin-bottom-30">
+    <table className="table table-hover">
+        <thead>
+            <tr>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">Rank</th>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">Username</th>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">Kills</th>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">Wins</th>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">K/D</th>
+                <th className="font-style letter-spacing-2 text-white text-center font-size-25" scope="col">Platform</th>
+            </tr>
+        </thead>
+        {innerTable}
+    </table>
+    </div>)
+
+    return table
+  }
+
+  function isTopThree(elem){
     if(elem == "1"){
-        return "notAllowDisplay";
+        return "first";
+    } else if(elem == "2"){
+        return "second";
+    } else if(elem == "3"){
+        return "third";
     } else {
-        return "allowDisplay";
+        return "defaultLeaderboard";
     }
-   }
+  }
 
 
   if (error) {
@@ -86,34 +182,15 @@ const BrDailyStore = ({ data, isLoading, error }) => {
           <h1 className="font-style text-center text-white">Leaderboard</h1>
           <hr className="hr margin-bottom-30" />
         </div>
-        <div className="col-lg-12">
-          <p className="font-style font-size-20 text-white" style={{float: 'right'}}>Top 10</p>
-        </div>
       </div>
       <div className="row margin-bottom-30">
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th className="font-style text-white text-center" scope="col">#</th>
-                    <th className="font-style text-white text-center" scope="col">Username</th>
-                    <th className="font-style text-white text-center" scope="col">Kills</th>
-                    <th className="font-style text-white text-center" scope="col">Wins</th>
-                </tr>
-            </thead>
-        {hits.map(hit =>
-            <tbody>
-                <tr className={isTopThree(hit.rank)}>
-                    <th className="font-style text-center" scope="row"><p className={allowDisplayTxt(hit.rank)}>{hit.rank}</p><img className={allowDisplayImg(hit.rank)} src={firstPlace} style={{width: '90px'}}/></th>
-                    <td className="font-style text-center">{hit.username}</td>
-                    <td className="font-style text-center">{hit.kills}</td>
-                    <td className="font-style text-center">{hit.wins}</td>
-                </tr>
-            </tbody>
-        )}
-        </table>
+      {firstPlace(hits)}
+      {secondPlace(hits)}
+      {thirdPlace(hits)}
       </div>
+      {otherPlayer(hits)}
     </div>
   );
 }
 
-export default withFetching(API + DEFAULT_QUERY)(BrDailyStore);
+export default withFetching(API + DEFAULT_QUERY)(top10);
