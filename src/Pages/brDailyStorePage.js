d23aff3ecf924891328e0247c3d497639caaccb6
ilkem.erol <ilkem.erol@hotmail.com>
2018-07-22 13:32:45 +0300

    brDailyStore Date Impl.

--- src/Pages/brDailyStorePage.js
+++ src/Pages/brDailyStorePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import '../css/bootstrap.css';
 import '../App.css';
 import BrDailyStore from '../Apis/brDailyStore';
