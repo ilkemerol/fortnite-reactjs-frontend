5c97aba75ee1aa6b4bc17eda1f35ba8da3c529c2
Ilkem Erol <ilkemerol@Ilkems-MacBook-Pro.local>
2018-07-28 23:43:01 +0300

    Weekly Mission Impl

---
+++ src/Pages/weeklyMissionsPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/bootstrap.css';
+import '../App.css';
+
+
+class weeklyMissionsPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>TO DO</p>
+      </div>
+    );
+  }
+}
+
+export default weeklyMissionsPage;
